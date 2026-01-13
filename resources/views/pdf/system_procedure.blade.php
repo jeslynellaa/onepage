785d6fa77bd38f3e16aa3dc06c5fb6e7898e779e
Jenn Ella <jeslynella@gmail.com>
2026-01-13 13:21:59 +0800

    fixed layout of system procedure

--- resources/views/pdf/system_procedure.blade.php
+++ resources/views/pdf/system_procedure.blade.php
@@ -7,14 +7,14 @@
 
     <style>
         @page {
-            margin: 75px 65px 30px 65px; /* top, right, bottom, left */
+            margin: 75px 65px 50px 65px; /* top, right, bottom, left */
         }
         body {
             font-family: Helvetica, sans-serif;
-            font-size: 13px;
+            font-size: 11pt;
             margin: 0;
-            padding-top: 170px;   /* same as header height */
-            padding-bottom: 30px; /* same as footer height */
+            padding-top: 180px;   /* same as header height */
+            padding-bottom: 20px; /* same as footer height */
         }
         header {
             position: fixed;
@@ -30,15 +30,15 @@
         }
         footer {
             position: fixed;
-            bottom: -30px; /* adjust depending on your @page bottom margin */
+            bottom: -50px; /* adjust depending on your @page bottom margin */
             left: 0;
             right: 0;
-            height: 30px;
+            height: 50px;
 
             /* Optional styling */
             text-align: center;
-            /* border: 1px solid black; */
             padding-top: 5px;
+            font-size: 8pt;
         }
         .break{
             page-break-before: always;
@@ -63,30 +63,30 @@
         #manual_name,
         #process_table thead tr{
             font-weight: 700;
-            font-size: 15px;
+            font-size: 11pt;
             background-color: lightblue;
             text-align: center;
         }
         #title-row{
             font-weight: 700;
-            font-size: 15px;
+            font-size: 14pt;
             text-align: center;
             text-transform: uppercase;
         }
         #scope_objectives_table tbody tr td:nth-child(2){
-            width: 75%;
+            width: 80%;
         }
         #scope_objectives_table ul{
             padding-left: 10px;
             margin: 0;
         }
         #process_table tbody{
-            font-size: 12px;
+            font-size: 10pt;
             text-align: center;
         }
         .arrow-line {
             width: 0;
-            height: 15px;
+            height: 12px;
             border-left: 3px solid black;
             border-right: 3px solid black;
             border-top: 3px solid #000;
@@ -103,6 +103,8 @@
         #process_table tbody tr td{
             padding-top: 0;
             padding-bottom: 0;
+            padding-left: 5px;
+            padding-right: 5px;
             margin-top: 0;
             margin-bottom: 0;
         }
@@ -134,6 +136,23 @@
         td ol{
             padding-left: 1rem;
         }
+        .connector{
+            margin-right: auto;
+            margin-left: auto;
+            width:45px;
+            height:45px;
+            background-size: 45px 45px;
+            background-repeat:no-repeat;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            font-size:14pt;
+            padding-top: 5px;
+        }
+        ul li{
+            padding: 0;
+            margin: 0;
+        }
     </style>
 </head>
 <body>
@@ -144,13 +163,13 @@
                     <td colspan="3">SYSTEM PROCEDURES MANUAL</td>
                 </tr>
                 <tr>
-                    <td rowspan="5" style="font-size:14px; text-align:center"><strong>NAME OF COMPANY</strong></td>
+                    <td rowspan="5" style="font-size:14pt; text-align:center"><strong>FCU SOLUTIONS INC</strong></td>
                     <td class="label_cell">Section No.:</td>
                     <td class="data_cell">{{ $doc->section_number }}</td>
                 </tr>
                 <tr>
                     <td class="label_cell">Revision No.:</td>
-                    <td class="data_cell">{{ $doc->revision_number }}</td>
+                    <td class="data_cell">{{ $doc->revision_number ?? "-" }}</td>
                 </tr>
                 <tr>
                     <td class="label_cell">Document No.:</td>
@@ -158,7 +177,7 @@
                 </tr>
                 <tr>
                     <td class="label_cell">Effective Date:</td>
-                    <td class="data_cell">{{ date("m/d/Y", strtotime($doc->effective_date)) }}</td>
+                    <td class="data_cell">{{ $doc->effective_date ? date("m/d/Y", strtotime($doc->effective_date)) : "-"}}</td>
                 </tr>
                 <tr>
                     <td class="label_cell">Page Number:</td>
@@ -169,17 +188,18 @@
                 </tr>
             </tbody>
         </table>
-        <span style="font-size: 9px; font-style: italic;"><strong>STRICTLY CONFIDENTIAL</strong> - For use of [NAME OF COMPANY] only. Unauthorized reproduction is strictly prohibited.</span>
+        <span style="font-size: 9pt; font-style: italic;"><strong>STRICTLY CONFIDENTIAL</strong> - For use of FCU Solutions Inc only. Unauthorized reproduction is strictly prohibited.</span>
     </header>
 
     <footer>
+        <span style="font-style:italic; font-weight: bold;">Except for the MASTER COPY, printed and downloaded copies of this documented information are considered uncontrolled.</span>
     </footer>
 
     <main>
         <table id="scope_objectives_table">
             <tbody>
                 <tr>
-                    <td><strong>OBJECTIVES</strong></td>
+                    <td><strong>OBJECTIVE/S</strong></td>
                     <td style="text-align: justify;">{!! $doc->objective !!}</td>
                 </tr>
                 <tr>
@@ -193,43 +213,88 @@
             $note = 1;
         @endphp
 
-        <table id="process_table" style="margin-top: 20px; margin-bottom: 10px">
+        <table id="process_table" style="margin-top: 20px;">
             <thead>
                 <tr>
                     <th>Responsibility</th>
-                    <th style="width: 35%;">Activities</th>
+                    <th style="width: 40%;">Activities</th>
                     <th></th>
                     <th>Interfaces</th>
                 </tr>
             </thead>
             <tbody>
+                <tr>
+                    <td colspan="4" style="height:5px;"></td>
+                </tr>
                 @foreach ($steps as $key => $step)
+                @if ($key !== 0 && $key % 5 === 0)
+                    {{-- connector before breaking --}}
+                    <tr style="border-bottom: 1px solid black;">
+                        <td></td>
+                        <td style="text-align:center;">
+                            <div class="connector" style="background-image: url('data:image/png;base64,{{ base64_encode(file_get_contents($connector)) }}');">
+                                A
+                            </div>
+                        </td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+
+                    {{-- force page break --}}
+                    <tr style="page-break-before: always;"></tr>
+
+                    {{-- connector at top of new page --}}
+                    <tr>
+                        <td></td>
+                        <td style="text-align:center;">
+                            <div class="connector" style="margin-top: 8px; background-image: url('data:image/png;base64,{{ base64_encode(file_get_contents($connector)) }}');">
+                                A
+                            </div>
+                        </td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <div class="arrow-line" style="margin-top:0; margin-bottom: 0"></div>
+                            <div class="arrow-down" style="margin-top:0; margin-bottom: 0"></div>
+                        </td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                @endif
                 <tr>
                     <td>{{$step->responsibility}} </td>
-                    <td style="width: 35%;">
-                        @if ($key !== 0)
-                        <div class="arrow-line" style="margin-top:0; margin-bottom: 0"></div>
-                        <div class="arrow-down" style="margin-top:0; margin-bottom: 0"></div>
-                        @endif
-                        <p style="min-height: 50px; border: 1px solid black; padding: 10px; margin-bottom: 0; margin-top:0; vertical-align: middle">
-                            {{$step->activities}}
-                        </p>
-                        @if ($key+1 !== $steps->count())
-                        <div class="arrow-line" style="margin-top :0; margin-bottom: 0"></div>
-                        @endif
+                    <td style="height: 70px; width: 35%; border: 1px solid black;">
+                        {{$step->activities}}
                     </td>
-                    <td>
+                    <td style="padding-left:5px; padding-right:5px;">
                         @if ($step->note)
                             Note {{$note++}}
                         @endif
                     </td>
-                    <td style="width: 30%;">
+                    <td style="width: 30%; padding-left:5px; padding-right:5px;">
                         @foreach($step->interfaces as $interface)
                             {{$interface->category}}: {{$interface->title}} <br>
                         @endforeach
                     </td>
                 </tr>
+                @if($key+1 !== count($steps))
+                <tr>
+                    <td></td>
+                    <td>
+                        <div class="arrow-line" style="margin-top:0; margin-bottom: 0"></div>
+                        <div class="arrow-down" style="margin-top:0; margin-bottom: 0"></div>
+                    </td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                @endif
                 @endforeach
+                <tr>
+                    <td colspan="4" style="height:10px;"></td>
+                </tr>
             </tbody>
         </table>
         
@@ -239,7 +304,7 @@
 
         @foreach ($steps as $step_note)
             @if (!empty($step_note->note))
-                <div style="margin-bottom: 0.5rem">
+                <div style="margin-bottom: 0.5rem; page-break-inside: avoid;">
                     <p style="text-align: justify;">
                         <strong>Note {{$note_num++}}: </strong>
                         {!! $step_note->note !!}
@@ -248,17 +313,17 @@
             @endif
         @endforeach
 
-        <table id="interfaces_table">
+        <table id="interfaces_table" style="page-break-inside: avoid;">
             <thead>
                 <tr>
-                    <th style="border: 1px solid black; padding: 5px; height: 1.5rem">Documented Information Generated</th>
-                    <th style="border: 1px solid black; padding: 5px; height: 1.5rem">References</th>
+                    <th style="border: 1px solid black; padding: 3px;">Documented Information Generated</th>
+                    <th style="border: 1px solid black; padding: 3px;">References</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>
-                        <ol>
+                <tr style="padding-top:0">
+                    <td style="padding-top:0">
+                        <ol style="padding-top:0">
                             @foreach ($uniqueOutputs  as $output)
                             <li>{{ $output->category }}: {{ $output->title }} </li>
                             @endforeach
@@ -288,12 +353,12 @@
                         Position
                     </td>
                     <td style="height: 80px; vertical-align: bottom;">
-                        Name <br>
-                        Position
+                        Staff Reviewer<br>
+                        Reviewer
                     </td>
                     <td style="height: 80px; vertical-align: bottom;">
-                        Name <br>
-                        Position
+                        Staff Approver<br>
+                        Approver
                     </td>
                 </tr>
                 <tr>
