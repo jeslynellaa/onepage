6d2bf7af1ad5419d2f52713ef2cb213c33cac1ef
Jenn Ella <jeslynella@gmail.com>
2026-01-13 13:23:32 +0800

    made last activity dynamic

--- resources/views/document/index.blade.php
+++ resources/views/document/index.blade.php
@@ -15,12 +15,12 @@
                     </div>
                     <div class="flex-1">
                         <h3 class="text-xl font-semibold text-gray-800 m-0 p-0">System Procedures</h3>
-                        <p class="p-0 text-sm">Updated a week ago</p>
+                        <p class="p-0 text-sm">Last activity on {{$lastActivity}}</p>
                         <span class="bg-green-200 rounded-md px-3 text-sm">Active</span>
                     </div>
                 </a>
             </div>
-            <div class="w-full sm:w-1/2 md:w-1/3 px-2 mb-3 h-32">
+            {{-- <div class="w-full sm:w-1/2 md:w-1/3 px-2 mb-3 h-32">
                 <a href="{{ route('document.system_procedures') }}" 
                 class="flex items-center py-5 px-8 bg-white rounded-3xl shadow hover:shadow-lg transition-all duration-300 h-full">
                     <div class="text-5xl mr-5">
@@ -45,7 +45,7 @@
                         <span class="bg-green-200 rounded-md px-3 text-sm">Active</span>
                     </div>
                 </a>
-            </div>
+            </div> --}}
         </div>
     </div>
 </x-layout>
