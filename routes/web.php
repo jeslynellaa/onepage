46318e6db143450099f9d997f7a1420b550c10ad
Jenn Ella <jeslynella@gmail.com>
2026-01-29 16:57:09 +0800

    blocks logged in users from registration/ login pages

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,13 @@ use App\Http\Controllers\SectionsController;
 
 Route::get('/', [HomeController::class, 'index'])->name('dashboard')->middleware('auth');
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Example protected route
